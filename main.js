b6d30988f7d713e228735e45977b4da8e2d0f2fd
scastellontello-beep <scastellontello@gmail.com>
2026-09-22 11:20:55 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 onload = () =>{
     document.body.classList.remove("container");
 };
+document.addEventListener('DOMContentLoaded', function() {
+  const audio = document.getElementById('cancion');
+  document.body.addEventListener('click', function iniciarMusica() {
+    audio.play();
+    document.body.removeEventListener('click', iniciarMusica);
+  });
+});
